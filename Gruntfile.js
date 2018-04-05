405af0cdc686c658ac7f4a08cea1be7dfcff4c8f
JeanRemiDelteil <jeanremi.delteil@gmail.com>
2018-04-05 18:24:20 +0200

    Remove un-needed webstore build options for a simpler build

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
 	 * grunt build --target=dev
 	 * grunt build --target=prod
 	 *
-	 * grunt publishAddon --target=prod
+	 * grunt build_push --target=prod
 	 */
 		
 		// define target and config to run the task with
@@ -24,13 +24,6 @@ module.exports = function(grunt) {
 	 *     scriptId: string
 	 *   },
 	 *   script_manifest: {},
-	 *   publishing: {
-	 *     version: number,
-	 *     versionOffset: number,
-	 *     account: string,
-	 *     appID: string,
-	 *     manifest: Object
-	 *   }
 	 * }}
 	 */
 	let CONFIG;
@@ -181,18 +174,6 @@ module.exports = function(grunt) {
 				command: 'version',
 			},
 		},
-		zip: {
-			manifest: {}
-		},
-		webstore:{
-			'updateItem': {
-				/** Do publish or just update the draft */
-				publish: true,
-				
-				/** @type {'trustedTesters' | 'default'} */
-				target: 'trustedTesters'
-			}
-		}
 	});
 	
 	
@@ -339,176 +320,6 @@ module.exports = function(grunt) {
 				break;
 		}
 	});
-	// Update addon ZIP file
-	grunt.registerMultiTask('zip', 'Update addon zip file to prepare it for webstore deploy', function(){
-		const JSZip = require('jszip');
-		const fs = require('fs');
-		let done = this.async();
-		
-		let manifest = CONFIG.publishing.manifest;
-		
-		if (!CONFIG.publishing.version){
-			console.error(`The webstore draft can't be created with no script version`);
-			
-			return false;
-		}
-		
-		// Update script version
-		manifest['container_info'].container_version = `${CONFIG.publishing.version}`;
-		// Bump manifest version (as script are always versioned, this should suffice)
-		manifest.version = `${CONFIG.publishing.versionOffset + CONFIG.publishing.version}`;
-		
-		
-		let zip = new JSZip();
-		/**
-		 * @typedef {function} zip.file
-		 */
-		
-		new Promise((resolve, reject) => {
-			fs.readFile('build/addon_webstore.zip', (err, data) => {
-				if (err) reject(err);
-				
-				resolve(data);
-			});
-		})
-			.then(data => zip.loadAsync(data))
-			
-			// Update manifest.json & create zip
-			.then(() => {
-				// Update manifest.json
-				return zip.file('manifest.json', JSON.stringify(manifest))
-					// create Zip file
-					.generateAsync({type:"nodebuffer"})
-			})
-			
-			// Write to disk
-			.then(content => new Promise(resolve => {
-				fs.writeFile("build/addon_webstore_draft.zip", content, resolve);
-			}))
-			
-			// end task
-			.then(err => {
-				if (err) throw err;
-				
-				console.log('file create with success');
-				
-				done();
-			})
-			.catch(err => {
-				console.error(err);
-				
-				done(false);
-			})
-		
-	});
-	// Use webStore API
-	grunt.registerMultiTask('webstore', 'use Webstore API to send a draft and publish it', function(){
-		
-		// If we need to publish to unlisted, we need to update 
-		/*
-		 # In case of 'unlisted' target, only passing 'publishTarget' as a request header works
-		 if [[ ${PUBLISH_TARGET} == 'unlisted' ]]; then
-		 HTTP_CODE=$(curl \
-		 -w %{http_code} \
-		 -o api-response.json \
-		 -H "Authorization: Bearer $ACCESS_TOKEN"  \
-		 -H "x-goog-api-version: 2" \
-		 -H "Content-Length: 0" \
-		 -H "publishTarget: $PUBLISH_TARGET" \
-		 -X POST \
-		 -v https://www.googleapis.com/chromewebstore/v1.1/items/${EXTENSION_ID}/publish)
-		 # For other targets ('default' or 'trustedTesters') we must pass 'target' as request body in json format
-		 else
-		 HTTP_CODE=$(curl \
-		 -w %{http_code} \
-		 -o api-response.json \
-		 -H "Authorization: Bearer $ACCESS_TOKEN" \
-		 -H "x-goog-api-version: 2" \
-		 -H "Content-Type: application/json" \
-		 -d "{\"target\":\"$PUBLISH_TARGET\"}" \
-		 -X POST \
-		 -v https://www.googleapis.com/chromewebstore/v1.1/items/${EXTENSION_ID}/publish)
-		 fi
-		 */
-		
-		
-		const webstore = require('webstore-upload');
-		
-		const PUBLISH_DRAFT = this.data.publish || false;
-		const PUBLICATION_TARGET = this.data.target || 'trustedTesters' ;
-		
-		
-		if (!CONFIG.publishing.account){
-			console.error('No publishing account, please update the configuration');
-			return false;
-		}
-		
-		
-		// Load credentials
-		const credentials = require('./build/cred/client_secret');
-		const accountCred = credentials[CONFIG.publishing.account];
-		
-		if (!accountCred){
-			console.error('Publishing account not found in the credential file');
-			return false;
-		}
-		
-		
-		const uploadOptions = {
-			accounts: {
-				default: { //account under this section will be used by default 
-					//publish: true, //publish item right after uploading. default false 
-					client_id: accountCred['installed'].client_id,
-					client_secret: accountCred['installed'].client_secret,
-					refresh_token: accountCred.refresh_token
-				},
-				/*other_account: {
-				 publish: true, //publish item right after uploading. default false 
-				 client_id: 'ie204es2mninvnb.apps.googleusercontent.com',
-				 client_secret: 'LEJDeBHfS',
-				 refresh_token: '1/eeeeeeeeeeeeeeeeeeeeeee_aaaaaaaaaaaaaaaaaaa'
-				 },
-				 new_account: {
-				 cli_auth: true, // Use server-less cli prompt go get access token. Default false 
-				 publish: true, //publish item right after uploading. default false 
-				 client_id: 'kie204es2mninvnb.apps.googleusercontent.com',
-				 client_secret: 'EbDeHfShcj'
-				 }*/
-			},
-			extensions: {
-				addon: {
-					//required
-					appID: CONFIG.publishing.appID,
-					//required, we can use dir name and upload most recent zip file 
-					zip: 'build/addon_webstore_draft.zip',
-					publishTarget: PUBLICATION_TARGET,
-					publish: PUBLISH_DRAFT,
-				},
-				/*extension2: {
-				 account: 'new_account',
-				 //will rewrite values from 'account' section 
-				 publish: true,
-				 appID: 'jcbeonnlikcefedeaijjln',
-				 zip: 'test/files/test2.zip',
-				 publishTarget: 'trustedTesters'
-				 }*/
-			},
-			uploadExtensions : ['addon']
-		};
-		
-		let done = this.async();
-		
-		webstore(uploadOptions, 'default')['then'](() => {
-			console.log('Published with success');
-			
-			done();
-		})
-			.catch(() => {
-				console.error('Publishing failed');
-				
-				done(false);
-			});
-	});
 	
 	
 	// NEVER name the task as the configurator object
@@ -525,20 +336,10 @@ module.exports = function(grunt) {
 		'clasp:push',
 	]);
 	
-	grunt.registerTask('publishAddon', [
-		'clasp:push',
-		'clasp:version',
-		'zip:manifest',
-		'webstore:updateItem'
-	]);
 	
 	grunt.registerTask('build_push', [
 		'build',
 		'push',
-	]);
-	grunt.registerTask('build_publish', [
-		'build',
-		'publishAddon',
 	]);
 	
 	// define default task (for grunt alone)
